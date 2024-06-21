e23b1a996b95c3ca53476769cbb733e4c7008b4f
gmarinho21 <gabriel.jaka@gmail.com>
2024-06-21 16:33:45 -0400

    Changed dbpath to account for different databases

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_PATH}.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_PATH}.mongodb.net/${process.env.ENVIRONMENT}?retryWrites=true&w=majority`
   )
   .catch((error) => {
     mongoose.disconnect();
