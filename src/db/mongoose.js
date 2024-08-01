0a16bfd9fa1b351b7b7d817d887401170dcd7c46
gmarinho21 <gabriel.jaka@gmail.com>
2024-08-01 09:29:09 -0400

    fix: await

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-try {
-  await mongoose.connect(
+mongoose
+  .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_PATH}.mongodb.net/${process.env.ENVIRONMENT}?retryWrites=true&w=majority`
-  );
-  console.log("Connected to the DB");
-} catch (error) {
-  mongoose.disconnect();
-  console.log("Could not connect to the DB");
-}
+  )
+  .catch((error) => {
+    mongoose.disconnect();
+    console.log("Could not connect to the DB");
+  });
